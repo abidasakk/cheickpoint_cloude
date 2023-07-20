a82c7facf4f5fb1ab51162a120f65cdaac7037c0
abidasakk <tanhaxelkevin@gmail.com>
2023-07-20 11:41:30 +0200

    cloud

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function App() {
+
+  return ( 
+
+    <div className="App">
+     <h1> yopougon academy  </h1>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.5678857197518!2d-4.1041267999999995!3d5.3298832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1c1af3da53e1b%3A0x7ddec7d34aba342d!2scarrefour%20Lubafrique!5e0!3m2!1sfr!2sci!4v1689778991299!5m2!1sfr!2sci" className='cartes'></iframe>  
+
+    
+ 
+    </div>
+  ); 
+}
+
+export default App;
